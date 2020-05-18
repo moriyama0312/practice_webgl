8e27d47ef96ef04f39577841c77cc34ed428b496
Moriyama Masataka <masataka.312@gmail.com>
2020-05-18 22:41:50 +0900

    マトリクスの作成

--- js/script.js
+++ js/script.js
@@ -9,6 +9,19 @@ window.addEventListener('load', () => {
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
+	// matIVオブジェクトを生成
+	var m = new matIV();
+
+	// 各種行列の生成と初期化
+	var mMatrix = m.identity(m.create());   // モデル変換行列
+	var vMatrix = m.identity(m.create());   // ビュー変換行列
+	var pMatrix = m.identity(m.create());   // プロジェクション変換行列
+	var mvpMatrix = m.identity(m.create()); // 最終座標変換行列
+
+	// 各行列を掛け合わせる順序を示す一例
+	m.multiply(pMatrix, vMatrix, mvpMatrix); // p に v を掛ける
+	m.multiply(mvpMatrix, mMatrix, mvpMatrix); // さらに m を掛ける
+
 	console.log(gl);
 });
 
