a8ecee0c40663dc05fa0c2bb85b9025881370114
Moriyama Masataka <masataka.312@gmail.com>
2020-05-24 16:36:03 +0900

    階層の変化

--- js/script.js
+++ js/script.js
@@ -76,61 +76,61 @@ window.addEventListener('load', () => {
 	gl.drawArrays(gl.TRIANGLES, 0, 3);
 	
 	// コンテキストの再描画
-    gl.flush();
+	gl.flush();
+	
+	function create_shader(id) {
+		let shader;
+		let scriptElement = document.getElementById(id);
+	
+		if(!scriptElement) return;
+	
+		switch(scriptElement.type) {
+			case 'x-shader/x-vertex':
+				shader = gl.createShader(gl.VERTEX_SHADER);
+				break;
+			case 'x-shader/x-fragment':
+				shader = gl.createShader(gl.FRAGMENT_SHADER);
+				break;
+			default:
+				return;
+		}
+	
+		gl.shaderSource(shder, scriptElement.text);
+	
+		gl.compileShader(shader);
+	
+		if(gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+			return shader;
+		}else{
+			alert(gl.getShaderInfoLog(shader));
+		}
+	}
+	
+	function create_program(vs, fs) {
+		let program = gl.createProgram();
+	
+		gl.attachShader(program, vs);
+		gl.attachShader(program, fs);
+	
+		gl.linkProgram(program);
+	
+		if(gl.getProgramParameter(program, gl.LINK_STATUS)) {
+			gl.useProgram(program);
+			return program;
+		}else {
+			alert(gl.getProgramInfoLog(program));
+		}
+	}
+	
+	function create_vbo(data) {
+		let vbo = gl.createBuffer();
+	
+		gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+	
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+	
+		gl.bindBuffer(gl.ARRAY_BUFFER, null);
+	
+		return vbo;
+	}
 });
-
-function create_shader(id) {
-	let shader;
-	let scriptElement = document.getElementById(id);
-
-	if(!scriptElement) return;
-
-	switch(scriptElement.type) {
-		case 'x-shader/x-vertex':
-			shader = gl.createShader(gl.VERTEX_SHADER);
-			break;
-		case 'x-shader/x-fragment':
-			shader = gl.createShader(gl.FRAGMENT_SHADER);
-			break;
-		default:
-			return;
-	}
-
-	gl.shaderSource(shder, scriptElement.text);
-
-	gl.compileShader(shader);
-
-	if(gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-        return shader;
-    }else{
-        alert(gl.getShaderInfoLog(shader));
-    }
-}
-
-function create_program(vs, fs) {
-	let program = gl.createProgram();
-
-	gl.attachShader(program, vs);
-	gl.attachShader(program, fs);
-
-	gl.linkProgram(program);
-
-	if(gl.getProgramParameter(program, gl.LINK_STATUS)) {
-		gl.useProgram(program);
-		return program;
-	}else {
-		alert(gl.getProgramInfoLog(program));
-	}
-}
-
-function create_vbo(data) {
-	let vbo = gl.createBuffer();
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-	return vbo;
-}
